b4f82b7f4d91906bc2a87cda03148c1c3fb98247
hezw <1147863825@qq.com>
2018-08-17 19:47:28 +0800

    eurekaServer 提交

---
+++ CoolZhulApp/Common/src/com/coolzhul/common/util/StringUtil.java
@@ -0,0 +1,12 @@
+package com.coolzhul.common.util;
+
+/**
+ * StringUtil String的工具类
+ * @javaName StringUtil.java
+ * @Date 2018.02.15
+ * @author hezw
+ * @version 1.0
+ */
+public class StringUtil {
+
+}
---
+++ CoolZhulApp/GateWayServer/src/main/java/com/coolzhul/gateway/GateWayServerApplication.java
@@ -0,0 +1,11 @@
+package com.coolzhul.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(GateWayServerApplication.class,args);
+    }
+}
