4729a1435525d1ba6753cf74bb2e15785ca5e5d1
hezw <1147863825@qq.com>
2018-09-26 20:33:14 +0800

    提交

--- CoolZhulApp/Common/src/main/java/com/coolzhul/common/util/HttpUtil.java
+++ CoolZhulApp/Common/src/main/java/com/coolzhul/common/util/HttpUtil.java
@@ -5,14 +5,12 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
@@ -124,120 +122,6 @@ public class HttpUtil {
             return null;
         }
         /**
-         * 如果失败尝试3次
-         * @param url
-         * @param encoding
-         * @return
-         */
-        public static String tryGet(String url, String encoding){
-            String resultStr = "";
-            for (int i = 0; i < 3; i++) {
-                try {
-                    resultStr  = get(url, encoding);
-                    break;
-                } catch (Exception e) {
-                    logger.error("请求异常count:{} ",i,e);
-                }
-            }
-            return resultStr;
-        }
-        /**
-         * Post请求（默认超时时间）
-         * @param url
-         *
-         * @param encoding
-         * @return
-         */
-        public static String get(String url, String encoding)  throws IOException {
-            return get(url , null, CONNECTION_TIMEOUT, READ_DATA_TIMEOUT, encoding);
-        }
-        public static String get(String url, Map<String, String> cookies, String encoding) throws IOException {
-            return get(url , cookies, CONNECTION_TIMEOUT, READ_DATA_TIMEOUT, encoding);
-        }
-        public static String get(String url, Map<String, String> cookies,int timeout , String encoding) throws IOException {
-            return get(url, cookies, timeout, timeout, encoding);
-        }
-        public static String get(String url,Map<String, String> cookies ,int connectTimeout, int readTimeout, String encoding) throws IOException {
-            HttpGet get = new HttpGet(url);
-            RequestConfig requestConfig = RequestConfig.custom()
-                    .setSocketTimeout(readTimeout)
-                    .setConnectTimeout(connectTimeout)
-                    .setConnectionRequestTimeout(connectTimeout)
-                    .setExpectContinueEnabled(false).build();
-            get.setConfig(requestConfig);
-            if (cookies !=null && !cookies.isEmpty()){
-                StringBuilder buffer = new StringBuilder(128);
-                for (String cookieKey : cookies.keySet()){
-                    buffer.append(cookieKey).append("=").append(cookies.get(cookieKey)).append("; ");
-                }
-                // 设置cookie内容
-                get.setHeader(new BasicHeader("Cookie",buffer.toString()));
-            }
-            CloseableHttpResponse response = null;
-            if(url.startsWith("https")){//https
-                response = createSSLClient().execute(get);
-            }else{
-                response = httpclient.execute(get);
-            }
-            HttpEntity entity = response.getEntity();
-            try {
-                if(entity != null){
-                    String str = EntityUtils.toString(entity, encoding);
-                    return str;
-                }
-            }finally {
-                if(entity != null){
-                    entity.getContent().close();
-                }
-                if(response != null){
-                    response.close();
-                }
-            }
-
-            return null;
-        }
-       /* *//**
-         * sslClient
-         * @return
-         *//*
-        public static String postBody(String url, String body, String encoding, int connectTimeout, int readTimeout) throws IOException{
-            HttpPost post = new HttpPost(url);
-            RequestConfig requestConfig = RequestConfig.custom()
-                    .setSocketTimeout(readTimeout)
-                    .setConnectTimeout(connectTimeout)
-                    .setConnectionRequestTimeout(connectTimeout)
-                    .setExpectContinueEnabled(false).build();
-            post.setConfig(requestConfig);
-
-            if (StrKit.notBlank(body)) {
-                StringEntity formEntity = new StringEntity(body, encoding);
-                post.setEntity(formEntity);
-            }
-
-            CloseableHttpResponse response = null;
-            if(url.startsWith("https")){//https
-                response = createSSLClient().execute(post);
-            }else{
-                response = httpclient.execute(post);
-            }
-
-            HttpEntity entity = response.getEntity();
-            try {
-                if(entity != null){
-                    String str = EntityUtils.toString(entity, encoding);
-                    return str;
-                }
-            }finally {
-                if(entity != null){
-                    entity.getContent().close();
-                }
-                if(response != null){
-                    response.close();
-                }
-            }
-            return null;
-        }*/
-        /**
          * map转成queryStr
          * @param paramMap
          * @return
@@ -252,9 +136,9 @@ public class HttpUtil {
         }
 
         public static  void main(String[] args) throws IOException {
-            String uri = "https://cn.bing.com";
+            String uri="";
             String res = HttpUtil.post(uri,null,"utf-8");
-            System.out.println(res);
+            System.out.println("content:"+res);
         }
 
 }
