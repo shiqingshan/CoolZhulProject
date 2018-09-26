c090ceddbae58251ace7e309628253f80f2e0c9d
hezw <1147863825@qq.com>
2018-09-26 20:51:23 +0800

    提交

--- CoolZhulApp/Common/src/main/java/com/coolzhul/common/util/StringUtil.java
+++ CoolZhulApp/Common/src/main/java/com/coolzhul/common/util/StringUtil.java
@@ -2,11 +2,17 @@ package com.coolzhul.common.util;
 
 /**
  * StringUtil String的工具类
- * @javaName StringUtil.java
+ * @javaName StringUtil
  * @Date 2018.02.15
  * @author hezw
  * @version 1.0
  */
 public class StringUtil {
-
+    public boolean isNull(String str){
+        if(str==null||"".equals(str)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
